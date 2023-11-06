eecd5ba44ed50142e3498a46f170cd5448eb7be0
amendoz1 <147355883+amendoz1@users.noreply.github.com>
2023-11-06 00:44:48 -0800

    Update wave_capture.v

--- wave_capture.v
+++ wave_capture.v
@@ -23,12 +23,11 @@ reg[7:0] next_count;
 reg final_sample_written;
 wire pos_zero_cross;
 reg tempIndex;
-reg temp_address;
-reg [15:0] next_sample;
-reg [15:0] current_sample;
+reg [8:0] temp_address;
+wire [15:0] intermediate_sample;
+wire [15:0] current_sample;
 reg [7:0] temp_written;
 reg temp_enable_in;
-wire temp_enable_out;
 
 dff  #(3) ffState(.clk(clk), .d(next_state), .q(state)); //change enable (might not need one)
 dff ffIndex (.clk(clk), .d(tempIndex), .q(read_index));
@@ -43,7 +42,6 @@ always @(*) begin
     default: {next_state, temp_address, tempIndex} = {`ARMED, 8'd0, 1'b0};    //ARMED
     endcase
 end
-
 dffre #(8) ffCount(.clk(clk), .d(next_count), .q(count), .r(1'b0), .en(new_sample_ready)); // change enable (definitley need one here)
 // counter logic
 always @(*) begin
@@ -67,22 +65,24 @@ always @(*) begin
 assign write_address = temp_address;
 
 // pos zero cross logic
-dffr #(16) sampleFF(.clk(clk), .d(new_sample_in), .q(current_sample), .r(rst)); // need enable?
+dffre #(16) sampleFF1(.clk(clk), .d(new_sample_in), .q(intermediate_sample), .r(reset), .en(new_sample_ready)); // need enable? &&(state == `ACTIVE) under the assumption that we are only comparing the current sample to the previous sample, and we do not need to hold any of the samples (skipping samples is okay)
+dffre #(16) sampleFF2(.clk(clk), .d(intermediate_sample), .q(current_sample), .r(reset), .en(new_sample_ready));
+assign pos_zero_cross =  (new_sample_in[15] != 1'b1 && current_sample[15] == 1'b1);
 
-assign pos_zero_cross = (next_sample > 0 && current_sample < 0) ? 1'b1 : 1'b0; // this might be garbage at first because current_sample is also prolly gonna be garb
+//wondering if this is an issue because current sample is determined
+
 // writing address logic
-
-dff #(8) writtenSampleFF(.clk(clk), .d(temp_written), .q(write_sample)); // delete, will be writing the 8MSB of actual sample to write_sample, use assign 
+//dff #(8) writtenSampleFF(.clk(clk), .d(temp_written), .q(write_sample)); // delete, will be writing the 8MSB of actual sample to write_sample, use assign (DONT NEED FF?)
 always @(*) begin
     casex({reset, state})
     4'b1xxx :   {temp_written, temp_enable_in} = {8'd0, 1'b0};
-    4'b0010:   {temp_written, temp_enable_in} = {current_sample[7:0], 1'b1}; // (which sample should i grab the 8 bits from?), u
+    4'b0010:   {temp_written, temp_enable_in} = { ~ (current_sample[7:0]) + 1'b1 , 1'b1}; // (which sample should i grab the 8 bits from?), u
     default:   {temp_written, temp_enable_in} = {8'd0, 1'b0};
     endcase
 end
+assign write_sample = temp_written;
 
 // want write_enable = 1 when we have a new write_sample
-dffre write_enableFF(.clk(clk), .d(temp_enable_in), .q(temp_enable_out), .r(rst), .en(new_sample_ready));
+dffre write_enableFF(.clk(clk), .d(temp_enable_in), .q(write_enable), .r(reset), .en(new_sample_ready )); // enable because if a sample takes long to be sent, we should not enable writing of the same sample
 
-assign write_enable = one_pulse oneEnable(temp_enable_out);
 endmodule
